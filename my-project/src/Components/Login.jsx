251e851debfb70e080f1378f85b8f538e9f04573
irma <irmakaleem12@gmail.com>
2024-07-29 13:31:24 +0500

    TMS

---
+++ my-project/src/Components/Login.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+export const Login = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen overflow-hidden ">
+      <div className="relative flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0">
+        {/* left side */}
+        <div className="flex flex-col justify-center p-8 md:p-14">
+          <span className="mb-3 text-4xl font-bold">Welcome back</span>
+          <span className="font-light text-gray-400 mb-8">
+            Welcome back! Please enter your details
+          </span>
+          <div className="py-4">
+            <label htmlFor="email" className="mb-2 text-left text-md block">
+              Email
+            </label>
+            <input
+              type="text"
+              className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
+              name="email"
+              id="email"
+            />
+          </div>
+          <div className="py-4">
+            <label htmlFor="pass" className="mb-2 text-left text-md block">
+              Password
+            </label>
+            <input
+              type="password"
+              name="pass"
+              id="pass"
+              className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
+            />
+          </div>
+          <div className="flex justify-between w-full py-4">
+            <div className="mr-24">
+              <input type="checkbox" name="ch" id="ch" className="mr-2" />
+              <span className="text-md">Remember for 30 days</span>
+            </div>
+            <span className="font-bold text-md">Forgot password</span>
+          </div>
+          <button className="w-full bg-black text-white p-2 rounded-lg mb-6 hover:bg-white hover:text-black hover:border hover:border-gray-300">
+            Sign in
+          </button>
+          <div className="text-center text-gray-400">
+            Don't have an account?
+            <span className="font-bold text-black"> Sign up for free</span>
+          </div>
+        </div>
+        {/* right side */}
+        <div className="relative">
+          <img
+            src="./images/airplane.jpg"
+            alt="img"
+            className="w-[500px] h-full hidden rounded-r-2xl md:block object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
